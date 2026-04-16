52cf02a5c0bcc7feda16443ff99d714db37024d5
Wildan Scott Kennedy <afandikaw@gmail.com>
2026-04-16 15:50:33 +0800

    basicProgram

--- main.cpp
+++ main.cpp
@@ -3,10 +3,20 @@ using namespace std;
 
 int main () {
 
+    string nama;
+    int umur;
+
+    cout << "Masukkan nama anda: ";
+    getline (cin, nama);
+
+    cout << "Masukkan umur anda: ";
+    cin >> umur;
 
 
+    cout << "Nama anda adalah: " << nama << endl;
+    cout << "Umur anda adalah: " << umur << endl;
 
-    
+
 
     return 0;
 }
