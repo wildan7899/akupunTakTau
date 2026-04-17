13def3f43f7e2b1719d3a46bd2fac7f89fcf8596
Wildan Scott Kennedy <afandikaw@gmail.com>
2026-04-17 21:30:52 +0800

    Update...

--- main.cpp
+++ main.cpp
@@ -3,18 +3,34 @@ using namespace std;
 
 int main () {
 
-    string nama;
-    int umur;
-
-    cout << "Masukkan nama anda: ";
-    getline (cin, nama);
-
-    cout << "Masukkan umur anda: ";
-    cin >> umur;
+    int number;
+    char op;
 
 
-    cout << "Nama anda adalah: " << nama << endl;
-    cout << "Umur anda adalah: " << umur << endl;
+    cout << "Kalkulator BETA" << endl;
+    cout << "Masukkan angka pertama: ";
+    cin >> number;
+    cout << "Masukkan operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Masukkan angka kedua: ";
+    cin >> number;
+
+    if (op == '+') {
+        cout << "Hasil: " << number + number << endl;
+    } else if (op == '-') {
+        cout << "Hasil: " << number - number << endl;
+    } else if (op == '*') {
+        cout << "Hasil: " << number * number << endl;
+    } else if (op == '/') {
+        if (number != 0) {
+            cout << "Hasil: " << number / number << endl;
+        } else {
+            cout << "Error: Pembagian dengan nol tidak diperbolehkan." << endl;
+        }
+    } else {
+        cout << "Operator tidak valid!" << endl;
+    }
+    
 
 
 
